10f73312bb28ba92506e8c4092d94c60ede86459
SEONGBEOM-KIM <59245627+SEONGBEOM-KIM@users.noreply.github.com>
2020-08-11 14:42:30 +0900

    Login & Logout

--- users/views.py
+++ users/views.py
@@ -1,12 +1,26 @@
 from django.views import View
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
 
-        return render(request, "users/login.html")
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-    def post(self, request):
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
-        pass
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
